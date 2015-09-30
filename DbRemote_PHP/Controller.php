2e8b6a88cefc71000b98524136837aa6c2934a0c
alexBigBoss <armando.sepulvedacrz@gmail.com>
2015-09-29 22:45:14 -0500

    PHP
    Archivos necesarios para la conexión remota de BD

---
+++ DbRemote_PHP/Controller.php
@@ -0,0 +1,41 @@
+<?php
+
+// array for JSON response
+$response = array();
+ 
+// include db connect class
+require_once __DIR__ . '/DataBaseConnection.php';
+
+// connecting to db
+$db = new DataBaseConnection();
+ 
+// get all products from products table
+$result = mysql_query("SELECT * FROM empresas") or die(mysql_error());
+ 
+// check for empty result
+if (mysql_num_rows($result) > 0) {
+    // looping through all results
+    // products node
+    $response["empresas"] = array();
+ 
+    while ($row = mysql_fetch_array($result)) {
+        // temp user array
+        $product = array();
+        $product["id"] = $row["id"];
+        $product["nombre"] = $row["nombre"];
+        // push single product into final response array
+        array_push($response["empresas"], $product);
+    }
+    // success
+    $response["success"] = 1;
+ 
+    // echoing JSON response
+    echo json_encode($response);
+} else {
+    // no products found
+    $response["success"] = 0;
+    $response["message"] = "No products found";
+ 
+    // echo no users JSON
+    echo json_encode($response);
+}
---
+++ DbRemote_PHP/DataBaseConfig.php
@@ -0,0 +1,6 @@
+<?php
+
+define('DB_USER', "u651189321_fcfm"); 
+define('DB_PASSWORD', "cdis123"); 
+define('DB_DATABASE', "u651189321_bdrem"); 
+define('DB_SERVER', "localhost"); 
